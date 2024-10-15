37017ace3bd6863d2bc459d7b9dfeddac02ed9e6
Muhammad Suleman Khan <sulemankhan093@gmail.com>
2024-10-15 13:52:26 +0500

    see count exist getvalue setvalue and sql queries in frappe

--- demoapp/programming_module/doctype/server_side_scripting/server_side_scripting.py
+++ demoapp/programming_module/doctype/server_side_scripting/server_side_scripting.py
@@ -9,12 +9,51 @@ from frappe import _
 
 class ServerSideScripting(Document):
   
-	def validate(self):
-		self.get_value()
 
-	def get_value(self):
-		first_name, age = frappe.db.get_value('Clientside Scripting','PRE0005',['firstname','age'])
-		frappe.msgprint(_("The Parent Firstname is {0} and age is {1}").format(first_name,age))
+### Getting Values from document ###
 
-	
+	# def validate(self):
+		# self.get_value()
+
+	# def get_value(self):
+		# first_name, age = frappe.db.get_value('Clientside Scripting','PRE0005',['firstname','age'])
+		# frappe.msgprint(_("The Parent Firstname is {0} and age is {1}").format(first_name,age))
+
 		
+		### Setting Values of a doctype ###
+		# def validate(self):
+		# 	self.set_value()
+
+		# def set_value(self):	
+		# 	frappe.db.set_value('Clientside Scripting','PRE0005','age',25)
+		# 	first_name, age = frappe.db.get_value('Clientside Scripting','PRE0005',['firstname','age'])
+		# 	frappe.msgprint(_("The Parent Firstname is {0} and age is {1}").format(first_name,age))
+
+
+					### frappe db.exists method ###
+
+		# def validate(slef):
+		# 	if frappe.db.exists('Clientside Scripting','PRE0005'):
+		# 		frappe.msgprint('This document exists in database')
+		# 	else:
+		# 		frappe.msgprint('The document does not exist')
+    
+					### frappe db.count method method ###
+	# def validate(self):
+	# 	doc_count = frappe.db.count('Clientside Scripting',{'enable':1})
+	# 	frappe.msgprint(_("The Enable document count is {0}").format(doc_count))
+  
+
+					### frappe SQL Queries method ###
+  
+# 	def validate(self):
+# 				self.sql()    
+	
+# 	def sql(self):
+# 				data = frappe.db.sql("""
+# SELECT firstname,age FROM `tabClientside Scripting` Where enable = 1
+# """, as_dict=1)
+# 				for d in data:
+# 					frappe.msgprint(_("The Parent Firstname is {0} and age is {1}").format(d.firstname,d.age))
+
+				
